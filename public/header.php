272a51bfd7d0d0a58460124b69dddaced11e05f2
aguss1204 <putuagusputra@student.unud.ac.id>
2022-12-14 23:21:11 +0800

    upload

---
+++ public/header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tapops Hotel Reservation </title>
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Lora:400,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Cabin:400,500,600,700&display=swap" rel="stylesheet">
+
+  <!-- Css Styles -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+  <link rel="stylesheet" href="css/flaticon.css" type="text/css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+  <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+  <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
+  <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+  <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+  <!-- Page Preloder -->
+  <div id="preloder">
+    <div class="loader"></div>
+  </div>
+  <!-- Header Section Begin -->
+  <header class="header-section">
+    <div class="top-nav">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <ul class="tn-left">
+              <li><i class="fa fa-phone"></i> (0361) 140 2400</li>
+              <li><i class="fa fa-envelope"></i> tapopshotel@gmail.com</li>
+            </ul>
+          </div>
+          <div class="col-lg-6">
+            <div class="tn-right">
+              <div class="top-social">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-tripadvisor"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+              </div>
+              <a href="#" class="bk-btn">Booking Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="menu-item">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-2">
+            <div class="logo">
+              <a href="./home">
+                <img src="img/logo.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-10">
+            <div class="nav-menu">
+              <nav class="mainmenu">
+                <ul>
+                  <li class="active"><a href="./home">Home</a></li>
+                  <li><a href="./rooms">Rooms</a></li>
+                  <li><a href="./about-us">About Us</a></li>
+                  <li><a href="./contact">Contact</a></li>
+                </ul>
+              </nav>
+              <div class="nav-right search-switch">
+                <i class="icon_search"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Header End -->
---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/index', function () {
+    return view('home');
+});
+
+Route::get('/rooms', function () {
+    return view('rooms');
+});
+
+Route::get('/rooms-details', function () {
+    return view('room-details');
+});
+
+Route::get('/about-us', function () {
+    return view('about-us');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/blog-details', function () {
+    return view('blog-details');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
